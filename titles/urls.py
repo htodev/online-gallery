15eccab109960426457e8026febc82c9eb09fe7d
htodev <hristo.todeff@gmail.com>
2022-09-14 16:40:24 +0300

    add titles templates

--- titles/urls.py
+++ titles/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 
 # from titles import views
-from titles.views import detail_title
+from titles.views import detail_title, photo_detail
 
 urlpatterns = [
     path('title/<str:pk>/', detail_title, name='title-photos'),
+    path('title/photo/<str:pk>/', photo_detail, name='photo'),
 ]
--- titles/views.py
+++ titles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from photos.models import Photo
 from titles.models import Title
 
 
@@ -8,3 +9,10 @@ def detail_title(request, pk):
     photos = title.photo_set.all()
     contex = {'photos': photos}
     return render(request, 'titles/title_detail.html', contex)
+
+
+def photo_detail(request, pk):
+    photo = Photo.objects.get(id=pk)
+
+    contex = {'photo': photo}
+    return render(request, 'titles/photo_detail.html', contex)
