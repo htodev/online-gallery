03c88e9d25dd525b40de9f16d97745d26e7cb6e4
htodev <hristo.todeff@gmail.com>
2023-05-14 17:01:25 +0300

    small changes in view functuins

--- titles/views.py
+++ titles/views.py
@@ -7,23 +7,27 @@ from titles.models import Title
 
 def detail_title(request, pk):
     title = Title.objects.get(id=pk)
+    category_id = title.category.id if title.category else None
     photos = title.photo_set.all()
-    context = {'photos': photos}
+    context = {'photos': photos, 'title_id': category_id}
     return render(request, 'titles/title_detail.html', context)
 
 
 
 
+
+
 def photo_detail(request, pk):
-    photo = get_object_or_404(Photo, pk=pk)
-    title = photo.title
-    photos = title.photo_set.all()
-    photo_index = list(photos).index(photo)
-    previous_photo = photos[photo_index - 1] if photo_index > 0 else None
-    next_photo = photos[photo_index + 1] if photo_index < len(photos) - 1 else None
-    context = {
+   photo = get_object_or_404(Photo, pk=pk)
+   title = photo.title
+   photos = title.photo_set.all()
+   photo_index = list(photos).index(photo)
+   previous_photo = photos[photo_index - 1] if photo_index > 0 else None
+   next_photo = photos[photo_index + 1] if photo_index < len(photos) - 1 else None
+   context = {
        'photo': photo,
        'previous_photo': previous_photo,
        'next_photo': next_photo,
-    }
-    return render(request, 'titles/photo_detail.html', context)
+       'pk': title.id,
+   }
+   return render(request, 'titles/photo_detail.html', context)
